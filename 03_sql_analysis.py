f66b64f3d7120c101632c66469ce00d317e4ffbb
Alec Garza <alecmgarza@gmail.com>
2026-08-12 19:24:59 -0500

    Add SQL analysis query calculating rolling average over 10 movies

--- 03_sql_analysis.py
+++ 03_sql_analysis.py
@@ -3,7 +3,7 @@ import sqlite3
 
 conn = sqlite3.connect('data/letterboxd.db')
 
-query = '''
+query_1 = '''
 SELECT
     CASE
         WHEN Rewatch IS NULL THEN 'First Watch'
@@ -15,7 +15,25 @@ FROM diary
 GROUP BY watch_type;
 '''
 
-df = pd.read_sql(query, conn)
+df = pd.read_sql(query_1, conn)
+print('\n--- QUERY 1: FIRST WATCH VS. REWATCH ---\n')
 print(df)
+print('\n')
+
+query_2 = '''
+SELECT
+    "Watched Date" AS watched_date,
+    Name AS film_name,
+    Rating AS rating,
+    ROUND(AVG(Rating) OVER (
+        ORDER BY "Watched Date"
+        ROWS BETWEEN 9 PRECEDING AND CURRENT ROW
+    ), 2) AS rolling_avg
+FROM diary;
+'''
+
+df_rolling = pd.read_sql(query_2, conn)
+print('--- QUERY 2: ROLLING 10-MOVIE AVERAGE ---\n')
+print(df_rolling.head(15))
 
 conn.close()
